cc843092707fde7578f9d1720a7e4ffa6926acf7
Frank Bi <frank@frankbi.com>
2013-07-16 17:30:46 -0400

    plenty of TODOs on here

--- fulbright_scrape.py
+++ fulbright_scrape.py
@@ -2,6 +2,8 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+# TODO
+# Loop through the different starts
 url = 'http://us.fulbrightonline.org/component/filter'
 payload = {'view': 'searchresult', \
 	'format': 'raw', \
@@ -15,9 +17,16 @@ r = requests.post(url, data=payload)
 soup = BeautifulSoup(r.text)
 # content = soup.findAll('td', attrs={'style': 'color:  #565758'})
 
+fulbright_info = []
 content = soup.findAll('p', style=re.compile("font-size: 24px;"))
 for rows in content:
 	if rows.string.strip() != "Search Results":
 		names = rows.string.strip()
-		print names
-
+		# TODO
+		# Need to cut down on whitespace between first and last names		
+		a = []
+		a.append(names)
+			# TODO
+			# Append the remaining information to a before push to 2D
+		fulbright_info.append(a)
+print fulbright_info[0]
