1437bc2c300aeb08cc65ebaefc406416b177d26f
Frank Bi <frank@frankbi.com>
2013-07-18 13:57:43 -0400

    y

--- fulbright_scrape.py
+++ fulbright_scrape.py
@@ -23,32 +23,40 @@ def getData(start):
 	soup = BeautifulSoup(r.text)
 	return soup
 
-soup = getData(0)
+def passData(soup):
+	content = soup.findAll('p', style=re.compile('font-size: 24px;'))
+	numItems = len(content)
+	numItems = numItems - 1
+	start = 0
+	end = 5
+	for rows in content:
+		if rows.string.strip() != 'Search Results':
+			name = rows.string.strip()
+			name = re.sub(r'\s+', ' ', name)	
+			scholar = []
+			scholar.append(name)
+			scholar_details = soup.findAll('td', style=re.compile('color: #565758'))
+			for person in scholar_details[start:end]:
+				scholar_info = person.string.strip()
+				scholar.append(scholar_info)
+				start += 1
+				end += 1
+			fulbright_info.append(scholar)
 
-content = soup.findAll('p', style=re.compile('font-size: 24px;'))
-print len(content)
-start = 0
-end = 5
-for rows in content:
-	if rows.string.strip() != 'Search Results':
-		name = rows.string.strip()
-		name = re.sub(r'\s+', ' ', name)	
-		scholar = []
-		scholar.append(name)
-		scholar_details = soup.findAll('td', style=re.compile('color: #565758'))
-		for person in scholar_details[start:end]:
-			scholar_info = person.string.strip()
-			scholar.append(scholar_info)
-			start += 1
-			end += 1
-		fulbright_info.append(scholar)
+	for i in range(0, numItems):
+		name = fulbright_info[i][0]
+		school = fulbright_info[i][1]
+		state = fulbright_info[i][2]
+		field = fulbright_info[i][3]
+		country = fulbright_info[i][4]
+		year = fulbright_info[i][5]
+		c.writerow([name,school,state,field,country,year])
 
-for i in range(0, 20):
-	name = fulbright_info[i][0]
-	school = fulbright_info[i][1]
-	state = fulbright_info[i][2]
-	field = fulbright_info[i][3]
-	country = fulbright_info[i][4]
-	year = fulbright_info[i][5]
-	# print name + " " + school + " " + state + " " + field + " " + country + " " + year
-	c.writerow([name,school,state,field,country,year])
+"""
+for x in xrange(0, 200, 20):
+	soup = getData(x)
+	passData(soup)
+"""
+
+soup = getData(80)
+passData(soup)
